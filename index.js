cd9ac1b36e8b2f3a2f053c87cbbe446f2e53503e
Nafisa-Tabassum-Mim <mim.afrin129@gmail.com>
2024-05-15 22:05:50 +0600

    search func added

--- index.js
+++ index.js
@@ -1,17 +1,29 @@
 const express = require('express')
 const cors = require('cors')
 require('dotenv').config()
+const jwt = require('jsonwebtoken')
+const cookieParser = require('cookie-parser')
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express()
 const port = process.env.PORT || 5000
 
 
 // middlewares
-app.use(cors())
+app.use(cors({
+    origin: [
+        'http://localhost:5173',
+        'https://volunteer-91281.web.app',
+        'https://volunteer-91281.firebaseapp.com'
+    ],
+    credentials: true
+}))
 app.use(express.json())
+app.use(cookieParser())
 
 
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.gnbvncz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -22,25 +34,77 @@ const client = new MongoClient(uri, {
     }
 });
 
+
+
+// middlewares
+const logger = (req, res, next) => {
+    console.log("log info - ", req.method, req.url)
+    next()
+}
+
+const verifyToken = (req, res, next) => {
+    // console.log(req.cookies)
+    const token = req?.cookies?.token;
+    console.log('token in the middleware -', token);
+    // no token available 
+    if (!token) {
+        return res.status(401).send({ message: 'unauthorized access - no token' })
+    }
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(401).send({ message: 'unauthorized access' })
+        }
+        req.user = decoded;
+        next();
+    })
+}
+const cookieOption = {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production" ? true : false,
+    sameSite: process.env.NODE_ENV === "production" ? "none" : "strict"
+}
+
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        // await client.connect();
 
 
         // for every data collection - all the volunteering post is here 
         const postCollection = client.db('volunteer-need').collection('post')
-
         // my own volunteering request 
         const requestCollection = client.db('volunteer-need').collection('request')
 
+        // auth related api 
+        app.post('/jwt', logger, async (req, res) => {
+            const user = req.body
+            console.log('user for token', user)
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+                expiresIn: '1h'
+            })
+            res.cookie('token', token, cookieOption).send({ success: true })
+        })
 
+        app.post('/logout', async (req, res) => {
+            const user = req.body
+            console.log('logging out', user)
+            res.clearCookie('token', { ...cookieOption, maxAge: 0 }).send({ success: true })
+        })
 
         app.get('/post', async (req, res) => {
+        
+            const search = req.query?.search
             let query = {}
+            if (search) {
+               query = { 
+                post_title: { $regex: search, $options: 'i' } ,}
+            }
+
             if (req.query?.organizer_email) {
                 query = { organizer_email: req.query.organizer_email }
             }
+            console.log(query)
+
             const cursor = postCollection.find(query).sort({ deadline: 1 })
             const result = await cursor.toArray()
             res.send(result)
@@ -82,10 +146,31 @@ async function run() {
             // console.log(newRequest);
             const result = await requestCollection.insertOne(newRequest);
             res.send(result);
+
+
+
         })
 
-        
-        app.get('/request', async (req, res) => {
+        // new one decrease the volunteer
+        app.patch('/post/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: new ObjectId(id) }
+            const updateDoc = { $inc: { volunteers_needed: -1 } }
+            const result = await postCollection.updateOne(query, updateDoc)
+            res.send(result)
+
+        })
+
+
+
+        app.get('/request', logger, verifyToken, async (req, res) => {
+            console.log("query email", req.query?.volunteer_email)
+            console.log("user email", req.user)
+            console.log('cook cookies', req.cookies)
+
+            if (req.user.email !== req.query?.volunteer_email) {
+                return res.status(403).send({ message: 'forbidden access!' })
+            }
             let query = {}
             if (req.query?.volunteer_email) {
                 query = { volunteer_email: req.query.volunteer_email }
@@ -120,8 +205,10 @@ async function run() {
         })
 
 
+
+
         // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
+        // await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
